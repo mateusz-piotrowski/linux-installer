8cb7163cdd707fd0028bc7ba246f5e899ebd0476
Mateusz Piotrowski <mateusz7piotrowski@gmail.com>
2020-12-22 23:27:02 +0100

    Add repository refresh and system update command

--- linux-notebook-installer.sh
+++ linux-notebook-installer.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 echo '# = = = = = = = = = = = = = = ='
 echo '# The Linux Notebook Installer'
 echo '# = = = = = = = = = = = = = = ='
 
 heading () {
-    echo "\n\n$1\n"
+    echo "\n$1\n"
 }
+
+heading "Upgrading system packages ..."
+sudo apt update
+sudo apt upgrade -y
